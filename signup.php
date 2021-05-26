c4a53e74c687ac5eaf6424ff13312b3683f0d5fb
etahamad <etahamad@icloud.com>
2021-05-26 10:36:11 +0200

    EOL

--- signup.php
+++ signup.php
@@ -1,5 +1,7 @@
 <?php
 	$gGuestID = $_POST['gGuestID'];
+	$gUserName = $_POST['gUserName'];
+	$gPassword = $_POST['gPassword'];
 	$gFirstName = $_POST['gFirstName'];
 	$gLastName = $_POST['gLastName'];
 	$gGender = $_POST['gGender'];
@@ -8,20 +10,29 @@
 	$gAddress = $_POST['gAddress'];
 	$gPhoneNumber = $_POST['gPhoneNumber'];
 	$gHomePhoneNumber = $_POST['gHomePhoneNumber'];
+	$gPassportID = $_POST['gPassportID'];
 	$gEmail = $_POST['gEmail'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('localhost','root','','hoteltest1');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into guestinfo(gGuestID, gFirstName, gLastName, gGender, gCountry, gCity, gAddress, gPhoneNumber, gHomePhoneNumber, gEmail) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-		$stmt -> bind_param("issssssiis", $gGuestID, $gFirstName, $gLastName, $gGender, $gCountry, $gCity, $gAddress, $gPhoneNumber, $gHomePhoneNumber, $gEmail);
+		$stmt = $conn->prepare("insert into guestinfo(gGuestID, gFirstName, gLastName, gGender, gCountry, gCity, gAddress, gPhoneNumber, gHomePhoneNumber, gPassportID, gEmail) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt -> bind_param("issssssiiss", $gGuestID, $gFirstName, $gLastName, $gGender, $gCountry, $gCity, $gAddress, $gPhoneNumber, $gHomePhoneNumber, $gPassportID, $gEmail);
 		$execval = $stmt -> execute();
+		echo "User ID: ";
+		echo $gGuestID;
+		echo " Registration successfully...";
+		
+	}
+	    $stmt = $conn->prepare("insert into GuestACCT(gGuestID, gUserName, gPassword) values(?, ?, ?)");
+		$stmt -> bind_param("iss", $gGuestID, $gUserName, $gPassword);
+		$execval = $stmt -> execute();
+		echo "Guest: ";
 		echo $gGuestID;
 		echo " Registration successfully...";
 		$stmt->close();
 		$conn->close();
-	}
 ?>
